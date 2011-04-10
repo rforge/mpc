31faf6839375baecd0e099ee54bd4b3fe496ceff
murray <murray@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-04-10 06:48:57 +0000

    Update copyright header, add a GetPrecision function.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mpc@17 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/mpc.R
+++ pkg/R/mpc.R
@@ -1,23 +1,35 @@
-# Copyright(c) 2011 Murray M. Stokely
+# Copyright 2011 Google Inc.
 #
-# This file is part of the R Multiple Precision Complex package, mpc.
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
 #
-# mpc is free software: you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# 02110-1301, USA
 #
 # http://mpc.r-forge.r-project.org/
 
 mpc <- function(z, precision) {
   stopifnot(is.numeric(precision))
-  .Call("as_mpc_numeric", z, as.integer(precision), PACKAGE="mpc")
+  if (length(z) == 1) {
+    .Call("R_mpc", z, as.integer(precision), PACKAGE="mpc")
+  } else {
+    unlist(sapply(z, .Call("R_mpc", z, as.integer(precision), PACKAGE="mpc")))
+  }
 }
 
 as.mpc <- function(z, precision=52) {
   stopifnot(is.numeric(precision))
   stopifnot(is.numeric(z) || is.complex(z))
-  .Call("as_mpc_numeric", z, as.integer(precision), PACKAGE="mpc")
+  .Call("R_mpc", z, as.integer(precision), PACKAGE="mpc")
 }
 
 is.mpc <- function(z) {
@@ -38,9 +50,12 @@ print.mpc <-function(x, ...) {
 }
 
 summary.mpc <- function(object, ...) {
+  precision <- .Call("R_mpc_get_prec", object, PACKAGE="mpc")
   cat("Output: ",
       .Call("print_mpc", object, PACKAGE="mpc"),
-      "\nPrecision: ", .Call("R_mpc_get_prec", object, PACKAGE="mpc"), "\n")
+      "\nReal Precision: ", precision[1],
+      "Imaginary Precision: ", precision[2],
+      "\n")
 }
 
 Conj.mpc <- function(z) {
@@ -77,4 +92,17 @@ log.mpc <- function(x, base=exp(1)) {
 
 exp.mpc <- function(x) { 
   .Call("R_mpc_exp", x, PACKAGE="mpc")
+}
+
+# Utilities for testing
+
+ulp <- function(x, y) {
+  stopifnot(is.numeric(x), is.numeric(y))
+  stopifnot(length(x) == length(y))
+  return(.Call("R_ulp", x, y, PACKAGE="mpc"))
+}
+
+GetPrecision <- function(z) {
+  stopifnot(inherits(z, "mpc"))
+  return(.Call("R_mpc_get_prec", z, PACKAGE="mpc"))
 }
